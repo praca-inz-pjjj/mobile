a703a8caebc6e95cbbb6b062d9fd959f0cbf7685
jkr02 <jakubkroczek02@gmail.com>
2024-10-24 14:39:08 +0200

    cosmetic changes

--- app/(parent)/pickup/[id].tsx
+++ app/(parent)/pickup/[id].tsx
@@ -6,10 +6,11 @@ import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import QRCode from "react-native-qrcode-svg";
+import { View } from "react-native";
 
 export default function TabTwoScreen() {
   const { id } = useLocalSearchParams();
-  const [qrCode, setQRCode] = useState(0);
+  const [qrCode, setQRCode] = useState(null);
   useEffect(() => {
     async function doRequest() {
       try {
@@ -25,24 +26,39 @@ export default function TabTwoScreen() {
   }, [])
 
   return (
-    <ScrollView>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">{`Pick up view ${qrCode}`}</ThemedText>
-        <QRCode value={String(qrCode)} size={200} color="black" backgroundColor="white" />
-      </ThemedView>
-    </ScrollView>
+    <ThemedView style={styles.container}>
+          {qrCode && 
+    <><ThemedText style={styles.headerText}>{`Widok odbioru`}</ThemedText><ThemedText style={styles.qrCodeText}>{qrCode}</ThemedText><QRCode value={String(qrCode)} size={300} color="black" backgroundColor="#F8F9FA" /></>
+    }
+    {!qrCode && <ThemedText style={styles.headerText}>{"Nie ma takiej permisji"}</ThemedText>}
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#F8F9FA',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  qrCodeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 15,
+  },
   headerImage: {
     color: "#808080",
     bottom: -90,
     left: -35,
     position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "column",
-    gap: 8,
-  },
+  }
 });
